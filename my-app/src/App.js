40e686afbf2756ae3d140a766f0bbcacf6e1b644
Steven <stevenpstansberry@gmail.com>
2024-07-28 00:34:45 -0700

    added navbar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar></Navbar>
     </div>
   );
 }
---
+++ my-app/src/components/Navbar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { AppBar,Toolbar,Typography,Button, IconButton, Icon } from '@mui/material';
+
+
+function Navbar() {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+          <Icon />
+        </IconButton>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          FitGraphPro
+        </Typography>
+        <Button color="inherit">Home</Button>
+        <Button color="inherit">Features</Button>
+        <Button color="inherit">About</Button>
+        <Button color="inherit">Contact</Button>
+      </Toolbar>
+    </AppBar>
+  );
+}
+
+export default Navbar;
