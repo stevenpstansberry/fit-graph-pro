4a83265603d06a8a73d178f4af4b292df0672e65
Steven <stevenpstansberry@gmail.com>
2024-07-28 01:31:23 -0700

    Navbar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import Navbar from './components/Navbar';
+import Home from './components/Home'
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
+      <Home></Home>
     </div>
   );
 }
