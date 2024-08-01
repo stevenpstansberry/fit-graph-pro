64553b0429da59ef81f469040b6baa204a2fb7e1
Steven <stevenpstansberry@gmail.com>
2024-07-31 17:55:22 -0700

    refactored pages and components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,17 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import Navbar from './components/Navbar';
-import Home from './components/Home'
 import Footer from './components/Footer'
 import Features from './components/Features';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-      <Navbar></Navbar>
-      <Home></Home>
-      <Features></Features>
-      <Footer></Footer>
+        <Home></Home>
     </div>
   );
 }
---
+++ my-app/src/pages/home.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Features from '../components/Features'
+import Footer from '../components/Footer'
+import Navbar from '../components/Navbar'
+
+import { Container, Typography, Box } from '@mui/material';
+
+function Home() {
+  return (
+    <Container>
+        <Navbar></Navbar>
+        <Container maxWidth="md">
+        <Box sx={{ my: 4 }}>
+            <Typography variant="h2" component="h1" gutterBottom>
+            Welcome to FitGraphPro
+            </Typography>
+            <Typography variant="h5" component="p" gutterBottom>
+            Your ultimate fitness tracking and graphing app!
+            </Typography>
+        </Box>
+
+        </Container>
+      <Features></Features>
+      <Footer></Footer>
+  
+    </Container>
+  );
+}
+
+export default Home;
