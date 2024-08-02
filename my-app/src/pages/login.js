a3b6cab7124ea46176cb9503b0d61a9a49bef723
Steven <stevenpstansberry@gmail.com>
2024-08-02 11:41:36 -0700

    added ui for login/register

--- my-app/src/pages/login.js
+++ my-app/src/pages/login.js
@@ -3,24 +3,15 @@ import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 
 import { Container, Typography, Box } from '@mui/material';
+import SignIn from '../components/SignIn';
 
 
 function Login() {
     return (
       <Container>
-          <Navbar></Navbar>
-          <Container maxWidth="md">
-            <Box sx={{ my: 4 }}>
-            <Typography variant="h2" component="h1" gutterBottom>
-              Welcome to Login
-              </Typography>
-              <Typography variant="h5" component="p" gutterBottom>
-              Your ultimate fitness tracking and graphing app!
-              </Typography>
-            </Box>
-          </Container>
+        <Navbar></Navbar>
+        <SignIn/>
         <Footer></Footer>
-    
       </Container>
     );
   }
--- my-app/src/pages/register.js
+++ my-app/src/pages/register.js
@@ -1,27 +1,19 @@
 import React from 'react';
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
+import SignUp from '../components/SignUp';
 
 import { Container, Typography, Box , FormControl, InputLabel, Input, FormHelperText} from '@mui/material';
 
 
 function Register() {
-    return (
+  return (
       <Container>
-          <Navbar></Navbar>
-          <Container maxWidth="md">
-            <Box sx={{ my: 4 }}>
-            <FormControl>
-              <InputLabel htmlFor="my-input">Email address</InputLabel>
-              <Input id="my-input" aria-describedby="my-helper-text" />
-              <FormHelperText id="my-helper-text">We'll never share your email.</FormHelperText>
-            </FormControl>
-            </Box>
-          </Container>
-        <Footer></Footer>
-    
+          <Navbar />
+              <SignUp />
+          <Footer />
       </Container>
-    );
-  }
+  );
+}
   
   export default Register;
