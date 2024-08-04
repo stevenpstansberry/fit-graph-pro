f0812a5789192395d483e8be52694eaca87f20be
Steven <stevenpstansberry@gmail.com>
2024-08-04 15:48:02 -0700

    added functions in authService

--- my-app/src/services/AuthService.js
+++ my-app/src/services/AuthService.js
@@ -1,10 +1,25 @@
 module.exports = {
     getUser: function () {
-        const user = sessionStorage.getItem('user'):
+        const user = sessionStorage.getItem('user');
         if (user === 'undefined' || !user) {
             return null;
         } else {
             return JSON.parse(user);
         }
+    },
+    
+    getToken: function() {
+        return sessionStorage.getItem('token');
+    },
+
+    setUserSession: function (user,token){
+        sessionStorage.setItem('user', JSON.stringify(user));
+        sessionStorage.setItem('token', token);
+    },
+    resetUserSession: function(){
+        sessionStorage.removeItem('user');
+        sessionStorage.removeItem('token');
     }
+
+
 }
