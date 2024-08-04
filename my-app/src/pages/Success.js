7bde39a8c07bd660d297a617469ef4f7ec2db26f
Steven <stevenpstansberry@gmail.com>
2024-08-04 16:09:52 -0700

    fixed missing import

--- my-app/src/pages/Success.js
+++ my-app/src/pages/Success.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Features from '../components/Features'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
+import { getUser, resetUserSession } from '../services/AuthService';
 
 import { Container, Typography, Box } from '@mui/material';
 
