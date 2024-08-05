01fc3d5b78d41da2248dce9d2f31cf8b1ed9adb2
Steven <stevenpstansberry@gmail.com>
2024-08-05 12:00:12 -0700

    changed navbar to show different buttons based on whether or not user is logged in

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -6,28 +6,55 @@ import { getUser} from '../services/AuthService';
 
 function Navbar() {
 
+  // Check to see whether or not the user is logged in or not.
   const user = getUser();
+  if (user){
+    
+    // If the user is logged in, show them appropiate navbar
+    return (
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+            <Icon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            FitGraphPro
+          </Typography>
+          <Button color="inherit" component = {Link} to ='/'>Home</Button>
+          <Button color="inherit" component = {Link} to ='/'>Features</Button>
+          <Button color="inherit" component = {Link} to ='/About'>About</Button>
+          <Button color="inherit" component = {Link} to ='/Workouts'>Workouts</Button>
+          <Button color="inherit" component = {Link} to ='/Contact'>Contact</Button>
+          <Button color="inherit" component = {Link} to ='/Success'>Profile</Button>
   
+        </Toolbar>
+      </AppBar>
+    );
+  } 
+  // if the user is not logged in, show them the appropiate navbar 
+  else {
+    return (
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+            <Icon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            FitGraphPro
+          </Typography>
+          <Button color="inherit" component = {Link} to ='/'>Home</Button>
+          <Button color="inherit" component = {Link} to ='/Login'>Login</Button>
+          <Button color="inherit" component = {Link} to ='/Register'>Register</Button>
+          <Button color="inherit" component = {Link} to ='/'>Features</Button>
+          <Button color="inherit" component = {Link} to ='/About'>About</Button>
+          <Button color="inherit" component = {Link} to ='/Contact'>Contact</Button>
   
-  return (
-    <AppBar position="static">
-      <Toolbar>
-        <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-          <Icon />
-        </IconButton>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          FitGraphPro
-        </Typography>
-        <Button color="inherit" component = {Link} to ='/'>Home</Button>
-        <Button color="inherit" component = {Link} to ='/Login'>Login</Button>
-        <Button color="inherit" component = {Link} to ='/'>Features</Button>
-        <Button color="inherit" component = {Link} to ='/About'>About</Button>
-        <Button color="inherit" component = {Link} to ='/Contact'>Contact</Button>
-        <Button color="inherit" component = {Link} to ='/Success'>Profile</Button>
+        </Toolbar>
+      </AppBar>
+    );
+  }
+  
 
-      </Toolbar>
-    </AppBar>
-  );
 }
 
 export default Navbar;
