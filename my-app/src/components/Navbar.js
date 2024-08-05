f881f38f6d7c73062089f3e10b989292d1030700
Steven <stevenpstansberry@gmail.com>
2024-08-05 13:12:33 -0700

    updated navbar to conditonally show pages as well as include settings / profile dropdown

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -1,60 +1,242 @@
-import React from 'react';
-import { AppBar,Toolbar,Typography,Button, IconButton, Icon } from '@mui/material';
 import {Link} from 'react-router-dom';
-import { getUser} from '../services/AuthService';
+import { getUser, resetUserSession} from '../services/AuthService';
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import AdbIcon from '@mui/icons-material/Adb';
+import {useNavigate} from 'react-router-dom';
 
 
-function Navbar() {
+const pages = ['Workouts', 'About', 'Contact'];
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
-  // Check to see whether or not the user is logged in or not.
+function Navbar()  {
   const user = getUser();
-  if (user){
+  //const pages = user ? ['Workouts', 'About', 'Contact'] :  ['Login', 'Register', 'About', 'Contact'];
+
+
+
+  const navigate = useNavigate();
+
+  
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  const logoutHandler = () => {
+    resetUserSession();
+    navigate('/Login');
+}
+
+return (
+  <AppBar position="static">
+    <Container maxWidth="xl">
+      <Toolbar disableGutters>
+        <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} href="/home" />
+        <Typography
+          variant="h6"
+          noWrap
+          component="a"
+          href="/home"
+          sx={{
+            mr: 2,
+            display: { xs: 'none', md: 'flex' },
+            fontFamily: 'monospace',
+            fontWeight: 700,
+            letterSpacing: '.3rem',
+            color: 'inherit',
+            textDecoration: 'none',
+          }}
+        >
+          Fit Graph
+        </Typography>
+
+        <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+          <IconButton
+            size="large"
+            aria-label="account of current user"
+            aria-controls="menu-appbar"
+            aria-haspopup="true"
+            onClick={handleOpenNavMenu}
+            color="inherit"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Menu
+            id="menu-appbar"
+            anchorEl={anchorElNav}
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'left',
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'left',
+            }}
+            open={Boolean(anchorElNav)}
+            onClose={handleCloseNavMenu}
+            sx={{
+              display: { xs: 'block', md: 'none' },
+            }}
+          >
+            {pages.map((page) => (
+              <MenuItem key={page} onClick={handleCloseNavMenu}>
+                <Typography textAlign="center">{page}</Typography>
+              </MenuItem>
+            ))}
+          </Menu>
+        </Box>
+        <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+        <Typography
+          variant="h5"
+          noWrap
+          component="a"
+          href="#app-bar-with-responsive-menu"
+          sx={{
+            mr: 2,
+            display: { xs: 'flex', md: 'none' },
+            flexGrow: 1,
+            fontFamily: 'monospace',
+            fontWeight: 700,
+            letterSpacing: '.3rem',
+            color: 'inherit',
+            textDecoration: 'none',
+          }}
+        >
+          LOGO
+        </Typography>
+        <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+          {pages.map((page) => (
+            <Button
+              key={page}
+              onClick={handleCloseNavMenu}
+              href = {page}
+              sx={{ my: 2, color: 'white', display: 'block' }}
+            >
+              {page}
+            </Button>
+          ))}
+        </Box>
+
+        <Box sx={{ flexGrow: 0 }}>
+          {!user ? (
+            <Button
+              color="inherit"
+              onClick={() => navigate('/Login')}
+              sx={{ my: 2, display: 'block' }}
+            >
+              Log In / Register
+            </Button>
+            
+          ) : (
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+              </IconButton>
+            </Tooltip>
+          )}
+          <Menu
+            sx={{ mt: '45px' }}
+            id="menu-appbar"
+            anchorEl={anchorElUser}
+            anchorOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            open={Boolean(anchorElUser)}
+            onClose={handleCloseUserMenu}
+          >
+            {settings.map((setting) => (
+              <MenuItem key={setting}  href = {setting} onClick={setting === 'Logout' ? logoutHandler : handleCloseUserMenu}>
+                <Typography textAlign="center">{setting}</Typography>
+              </MenuItem>
+            ))}
+          </Menu>
+        </Box>
+      </Toolbar>
+    </Container>
+  </AppBar>
+);
+}
+//   // Check to see whether or not the user is logged in or not.
+//   const user = getUser();
+//   if (user){
     
-    // If the user is logged in, show them appropiate navbar
-    return (
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-            <Icon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            FitGraphPro
-          </Typography>
-          <Button color="inherit" component = {Link} to ='/'>Home</Button>
-          <Button color="inherit" component = {Link} to ='/'>Features</Button>
-          <Button color="inherit" component = {Link} to ='/About'>About</Button>
-          <Button color="inherit" component = {Link} to ='/Workouts'>Workouts</Button>
-          <Button color="inherit" component = {Link} to ='/Contact'>Contact</Button>
-          <Button color="inherit" component = {Link} to ='/Success'>Profile</Button>
+//     // If the user is logged in, show them appropiate navbar
+//     return (
+//       <AppBar position="static" style = {{width: '100%'}}>
+//         <Toolbar>
+//           <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+//             <Icon />
+//           </IconButton>
+//           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+//             FitGraphPro
+//           </Typography>
+//           <Button color="inherit" component = {Link} to ='/'>Home</Button>
+//           <Button color="inherit" component = {Link} to ='/'>Features</Button>
+//           <Button color="inherit" component = {Link} to ='/About'>About</Button>
+//           <Button color="inherit" component = {Link} to ='/Workouts'>Workouts</Button>
+//           <Button color="inherit" component = {Link} to ='/Contact'>Contact</Button>
+//           <Button color="inherit" component = {Link} to ='/Success'>Profile</Button>
   
-        </Toolbar>
-      </AppBar>
-    );
-  } 
-  // if the user is not logged in, show them the appropiate navbar 
-  else {
-    return (
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-            <Icon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            FitGraphPro
-          </Typography>
-          <Button color="inherit" component = {Link} to ='/'>Home</Button>
-          <Button color="inherit" component = {Link} to ='/Login'>Login</Button>
-          <Button color="inherit" component = {Link} to ='/Register'>Register</Button>
-          <Button color="inherit" component = {Link} to ='/'>Features</Button>
-          <Button color="inherit" component = {Link} to ='/About'>About</Button>
-          <Button color="inherit" component = {Link} to ='/Contact'>Contact</Button>
+//         </Toolbar>
+//       </AppBar>
+//     );
+//   } 
+//   // if the user is not logged in, show them the appropiate navbar 
+//   else {
+//     return (
+//       <AppBar position="static" style = {{width: '100%'}}>
+//         <Toolbar >
+//           <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+//             <Icon />
+//           </IconButton>
+//           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+//             FitGraphPro
+//           </Typography>
+//           <Button color="inherit" component = {Link} to ='/'>Home</Button>
+//           <Button color="inherit" component = {Link} to ='/Login'>Login</Button>
+//           <Button color="inherit" component = {Link} to ='/Register'>Register</Button>
+//           <Button color="inherit" component = {Link} to ='/'>Features</Button>
+//           <Button color="inherit" component = {Link} to ='/About'>About</Button>
+//           <Button color="inherit" component = {Link} to ='/Contact'>Contact</Button>
   
-        </Toolbar>
-      </AppBar>
-    );
-  }
+//         </Toolbar>
+//       </AppBar>
+//     );
+//   }
   
 
-}
+// }
 
 export default Navbar;
