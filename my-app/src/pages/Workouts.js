73a97ba7fe29bc3918b1719c3a59c554650b8cdd
Steven <stevenpstansberry@gmail.com>
2024-08-16 15:00:32 -0700

    can now add splits

--- my-app/src/pages/Workouts.js
+++ my-app/src/pages/Workouts.js
@@ -62,7 +62,6 @@ function Workouts() {
     setIsCardVisible(false);
   };
 
-  // Handle opening and closing of the add workout dialog
   const handleAddDialogOpen = () => {
     setIsAddDialogOpen(true);
   };
@@ -90,6 +89,7 @@ function Workouts() {
     const updatedWorkouts = predefinedWorkouts.filter((_, i) => i !== index);
     setPredefinedWorkouts(updatedWorkouts);
   };
+
 
   // Filter workouts based on selected month and year
   const filteredWorkouts = workoutHistory.filter(workout => {
@@ -134,151 +134,151 @@ function Workouts() {
     );
   };
 
-  return (
-    <Container sx={{ minHeight: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
-      <Navbar />
+return (
+  <Container sx={{ minHeight: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
+    <Navbar />
 
-      <Box sx={{ flexGrow: 1, display: 'flex', flexDirection: 'column', alignItems: 'center', textAlign: 'center' }}>
-        <Typography variant="h4" component="p" sx={{ mb: 4 }}>
-          Your Workouts
-        </Typography>
+    <Box sx={{ flexGrow: 1, display: 'flex', flexDirection: 'column', alignItems: 'center', textAlign: 'center' }}>
+      <Typography variant="h4" component="p" sx={{ mb: 4 }}>
+        Your Workouts
+      </Typography>
 
-        {/* Sticky container for selectors and button */}
-        <Box
-          sx={{
-            position: 'sticky',
-            top: 0,
-            zIndex: 100,
-            backgroundColor: 'white',
-            padding: '10px 0',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            gap: 2,
-            mb: 4,
-            width: '100%',
-          }}
-        >
-          {!showGraph && (
-            <>
-              <Select
-                value={selectedMonth}
-                onChange={(e) => setSelectedMonth(e.target.value)}
-              >
-                {monthNames.map((month, index) => (
-                  <MenuItem key={index} value={index + 1}>
-                    {month}
-                  </MenuItem>
-                ))}
-              </Select>
-              <Select
-                value={selectedYear}
-                onChange={(e) => setSelectedYear(e.target.value)}
-              >
-                {[2023, 2024, 2025].map(year => (
-                  <MenuItem key={year} value={year}>
-                    {year}
-                  </MenuItem>
-                ))}
-              </Select>
-            </>
-          )}
-          {filteredWorkouts.length > 0 && (
-            <Button 
-              variant="contained" 
-              color="secondary" 
-              onClick={handleGraphButtonClick}
-              sx={{ padding: '8px 16px', fontSize: '14px' }}
+      {/* Sticky container for selectors and button */}
+      <Box
+        sx={{
+          position: 'sticky',
+          top: 0,
+          zIndex: 100,
+          backgroundColor: 'white',
+          padding: '10px 0',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          gap: 2,
+          mb: 4,
+          width: '100%',
+        }}
+      >
+        {!showGraph && (
+          <>
+            <Select
+              value={selectedMonth}
+              onChange={(e) => setSelectedMonth(e.target.value)}
             >
-              {showGraph ? 'View Workout History' : 'Graph This Month'}
-            </Button>
-          )}
-        </Box>
-
-        {/* Conditionally render either the workout cards or the graph */}
-        {showGraph ? (
-          <StrengthChart workoutHistory={workoutHistory} filteredWorkouts={filteredWorkouts} selectedMonth={monthNames[selectedMonth - 1]} selectedYear={selectedYear} />
-        ) : (
-          filteredWorkouts.length > 0 ? (
-            <Box sx={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center', gap: 2 }}>
-              {filteredWorkouts.map((workout, index) => (
-                <WorkoutCardPreview key={index} workout={workout} />
+              {monthNames.map((month, index) => (
+                <MenuItem key={index} value={index + 1}>
+                  {month}
+                </MenuItem>
               ))}
-            </Box>
-          ) : (
-            <Typography variant="h6" sx={{ mb: 4 }}>
-              No workouts for {monthNames[selectedMonth - 1]} {selectedYear}
-            </Typography>
-          )
+            </Select>
+            <Select
+              value={selectedYear}
+              onChange={(e) => setSelectedYear(e.target.value)}
+            >
+              {[2023, 2024, 2025].map(year => (
+                <MenuItem key={year} value={year}>
+                  {year}
+                </MenuItem>
+              ))}
+            </Select>
+          </>
+        )}
+        {filteredWorkouts.length > 0 && (
+          <Button 
+            variant="contained" 
+            color="secondary" 
+            onClick={handleGraphButtonClick}
+            sx={{ padding: '8px 16px', fontSize: '14px' }}
+          >
+            {showGraph ? 'View Workout History' : 'Graph This Month'}
+          </Button>
         )}
       </Box>
 
-      {/* Workout creation buttons and edit icon */}
-      {!showGraph && (
-        <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', mb: 4, gap: 2 }}>
-          <Box sx={{ display: 'flex', gap: 2 }}>
-            {/* Default Workout Button */}
-            <Button variant="contained" color="primary" onClick={() => toggleAddWorkoutCard([])} sx={{ padding: '10px 20px', fontSize: '16px' }}>
-              Add Default Workout
-            </Button>
-
-            {/* Dynamically generate predefined workout buttons */}
-            {predefinedWorkouts.map((workout, index) => (
-              <Button key={index} variant="contained" color="primary" onClick={() => toggleAddWorkoutCard(workout.exercises)} sx={{ padding: '10px 20px', fontSize: '16px' }}>
-                Add {workout.name}
-              </Button>
+      {/* Conditionally render either the workout cards or the graph */}
+      {showGraph ? (
+        <StrengthChart workoutHistory={workoutHistory} filteredWorkouts={filteredWorkouts} selectedMonth={monthNames[selectedMonth - 1]} selectedYear={selectedYear} />
+      ) : (
+        filteredWorkouts.length > 0 ? (
+          <Box sx={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center', gap: 2 }}>
+            {filteredWorkouts.map((workout, index) => (
+              <WorkoutCardPreview key={index} workout={workout} />
             ))}
           </Box>
-          {/* Single edit icon to the right of the buttons */}
-          <IconButton onClick={handleOpenEditDialog}>
-            <EditIcon />
-          </IconButton>
-        </Box>
+        ) : (
+          <Typography variant="h6" sx={{ mb: 4 }}>
+            No workouts for {monthNames[selectedMonth - 1]} {selectedYear}
+          </Typography>
+        )
       )}
+    </Box>
 
-      {/* Edit Dialog */}
-      <Dialog open={isEditDialogOpen} onClose={handleCloseEditDialog}>
-        <DialogTitle>Edit Workout Splits</DialogTitle>
-        <DialogContent>
-          <Typography variant="h6">Available Workout Splits</Typography>
+    {/* Workout creation buttons and edit icon */}
+    {!showGraph && (
+      <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', mb: 4, gap: 2 }}>
+        <Box sx={{ display: 'flex', gap: 2 }}>
+          {/* Default Workout Button */}
+          <Button variant="contained" color="primary" onClick={() => toggleAddWorkoutCard([])} sx={{ padding: '10px 20px', fontSize: '16px' }}>
+            Add Default Workout
+          </Button>
+
+          {/* Dynamically generate predefined workout buttons */}
           {predefinedWorkouts.map((workout, index) => (
-            <Box key={index} sx={{ display: 'flex', alignItems: 'center', mt: 2 }}>
-              <TextField
-                value={workout.name}
-                onChange={(e) => {
-                  const updatedSplits = [...predefinedWorkouts];
-                  updatedSplits[index].name = e.target.value;
-                  setPredefinedWorkouts(updatedSplits);
-                }}
-                sx={{ mr: 2 }}
-              />
-              <IconButton color="error" onClick={() => handleDeleteSplit(workout.name)}>
-                <DeleteIcon />
-              </IconButton>
-            </Box>
+            <Button key={index} variant="contained" color="primary" onClick={() => toggleAddWorkoutCard(workout.exercises)} sx={{ padding: '10px 20px', fontSize: '16px' }}>
+              Add {workout.name}
+            </Button>
           ))}
-        </DialogContent>
-          {/* Add New Split */}
-          <Box sx={{display: 'flex', alignItems: 'center', mt: 4}}>
-            <TextField>
-              label="New Workout Split"
-              value={newSplitName}
-              onChange={(e) => setNewSplitName(e.target.value)}
-              sx= {{mr : 2}}
-            </TextField>
-            <IconButton>
-              <AddIcon />
+        </Box>
+        {/* Single edit icon to the right of the buttons */}
+        <IconButton onClick={handleOpenEditDialog}>
+          <EditIcon />
+        </IconButton>
+      </Box>
+    )}
+
+    {/* Edit Dialog */}
+    <Dialog open={isEditDialogOpen} onClose={handleCloseEditDialog}>
+      <DialogTitle>Edit Workout Splits</DialogTitle>
+      <DialogContent>
+        <Typography variant="h6">Available Workout Splits</Typography>
+        {predefinedWorkouts.map((workout, index) => (
+          <Box key={index} sx={{ display: 'flex', alignItems: 'center', mt: 2 }}>
+            <TextField
+              value={workout.name}
+              onChange={(e) => {
+                const updatedSplits = [...predefinedWorkouts];
+                updatedSplits[index].name = e.target.value;
+                setPredefinedWorkouts(updatedSplits);
+              }}
+              sx={{ mr: 2 }}
+            />
+            <IconButton color="error" onClick={() => handleDeleteSplit(workout.name)}>
+              <DeleteIcon />
             </IconButton>
           </Box>
-        <DialogActions>
-          <Button onClick={handleCloseEditDialog} color="primary">Done</Button>
-        </DialogActions>
-      </Dialog>
+        ))}
+        {/* Add New Split */}
+        <Box sx={{ display: 'flex', alignItems: 'center', mt: 4 }}>
+          <TextField
+            label="New Workout Split"
+            value={newSplitName}
+            onChange={(e) => setNewSplitName(e.target.value)}
+            sx={{ mr: 2 }}
+          />
+          <IconButton onClick={handleAddNewSplit}>
+            <AddIcon />
+          </IconButton>
+        </Box>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleCloseEditDialog} color="primary">Done</Button>
+      </DialogActions>
+    </Dialog>
 
-      <Workout_Card open={isCardVisible} onClose={handleClose} preloadedExercises={selectedWorkout} />
-      <Footer />
-    </Container>
-  );
+    <Workout_Card open={isCardVisible} onClose={handleClose} preloadedExercises={selectedWorkout} />
+    <Footer />
+  </Container>
+);
 }
 
 // Sample predefined workouts
