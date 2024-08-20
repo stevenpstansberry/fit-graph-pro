ab4c444a08e5531c513c0187454d28d7d987f00b
Steven <stevenpstansberry@gmail.com>
2024-08-20 15:13:58 -0700

    edited navbar to properly encompass the screen

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -56,139 +56,91 @@ function Navbar() {
   };
 
   return (
-    <AppBar position="static" sx={{ backgroundColor: '#333333' }}> {/* Updated background color */}
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} href="/home" />
-          <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            href="/home"
-            sx={{
-              mr: 2,
-              display: { xs: 'none', md: 'flex' },
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            Fit Graph
-          </Typography>
+<AppBar position="static" sx={{ backgroundColor: '#333333' }}>
+  <Container maxWidth={false} sx={{ px: 2 }}>
+    <Toolbar disableGutters sx={{ justifyContent: 'flex-start' }}>
+      {/* Grouping the title and the navigation links */}
+      <Box sx={{ display: 'flex', alignItems: 'center' }}>
+        <AdbIcon sx={{ mr: 1 }} />
+        <Typography
+          variant="h6"
+          noWrap
+          component="a"
+          href="/home"
+          sx={{
+            fontFamily: 'monospace',
+            fontWeight: 700,
+            letterSpacing: '.3rem',
+            color: 'inherit',
+            textDecoration: 'none',
+            mr: 4, // Add spacing between the title and navigation links
+          }}
+        >
+          Fit Graph
+        </Typography>
 
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
+        {/* Navigation links placed immediately to the right of the title */}
+        <Box sx={{ display: 'flex' }}>
+          {pages.map((page) => (
+            <Button
+              key={page}
+              onClick={handleCloseNavMenu}
+              href={`/${page.toLowerCase()}`}
+              sx={{ color: 'white', mx: 1 }} // Adjust spacing as needed
             >
-              <MenuIcon />
+              {page}
+            </Button>
+          ))}
+        </Box>
+      </Box>
+
+      {/* Spacer to push the user profile or login/register button to the far right */}
+      <Box sx={{ flexGrow: 1 }} />
+
+      {/* Profile or login/register button */}
+      <Box sx={{ flexGrow: 0 }}>
+        {!user ? (
+          <Button
+            color="inherit"
+            onClick={() => navigate('/Login')}
+            sx={{ my: 2, display: 'block' }}
+          >
+            Log In / Register
+          </Button>
+        ) : (
+          <Tooltip title="Open settings">
+            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+              <Avatar alt="avatar" src="/static/images/avatar/2.jpg" />
             </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: 'block', md: 'none' },
-              }}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-          <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
-          <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            href="#app-bar-with-responsive-menu"
-            sx={{
-              mr: 2,
-              display: { xs: 'flex', md: 'none' },
-              flexGrow: 1,
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            LOGO
-          </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-            {pages.map((page) => (
-              <Button
-                key={page}
-                onClick={handleCloseNavMenu}
-                href={`/${page.toLowerCase()}`}
-                sx={{ my: 2, color: 'white', display: 'block' }}
-              >
-                {page}
-              </Button>
-            ))}
-          </Box>
+          </Tooltip>
+        )}
+        <Menu
+          sx={{ mt: '45px' }}
+          id="menu-appbar"
+          anchorEl={anchorElUser}
+          anchorOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+          }}
+          keepMounted
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+          }}
+          open={Boolean(anchorElUser)}
+          onClose={handleCloseUserMenu}
+        >
+          {settings.map((setting) => (
+            <MenuItem key={setting} onClick={() => handleMenuClick(setting)}>
+              <Typography textAlign="center">{setting}</Typography>
+            </MenuItem>
+          ))}
+        </Menu>
+      </Box>
+    </Toolbar>
+  </Container>
+</AppBar>
 
-          <Box sx={{ flexGrow: 0 }}>
-            {!user ? (
-              <Button
-                color="inherit"
-                onClick={() => navigate('/Login')}
-                sx={{ my: 2, display: 'block' }}
-              >
-                Log In / Register
-              </Button>
-
-            ) : (
-              <Tooltip title="Open settings">
-                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                  <Avatar alt="avatar" src="/static/images/avatar/2.jpg" />
-                </IconButton>
-              </Tooltip>
-            )}
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={() => handleMenuClick(setting)}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-        </Toolbar>
-      </Container>
-    </AppBar>
   );
 }
 
