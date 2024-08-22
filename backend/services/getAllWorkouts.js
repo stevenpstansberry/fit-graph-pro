ea9971c10a6dbea00b97828a31565b95ea32d29c
Steven <stevenpstansberry@gmail.com>
2024-08-21 20:55:05 -0700

    edited getAllWorkouts.js

--- backend/services/getAllWorkouts.js
+++ backend/services/getAllWorkouts.js
@@ -5,30 +5,27 @@ AWS.config.update({
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 const workoutTable = 'fit-graph-user-workouts';
-const util = require('./utils/util'); 
+const util = require('../utils/util');
 
 async function getAllWorkouts() {
-  const params = {
-    TableName: workoutTable
-  };
-
-  try {
-    const result = await dynamodb.scan(params).promise();
-    
-    if (!result.Items || result.Items.length === 0) {
-      // Return a 404 response if no workouts are found
-      return util.buildResponse(404, { message: 'No workouts found' });
+    const params = {
+      TableName: workoutTable
+    };
+  
+    try {
+      const result = await dynamodb.scan(params).promise();
+  
+      if (!result.Items || result.Items.length === 0) {
+        return util.buildResponse(404, { message: 'No workouts found' });
+      }
+  
+      return util.buildResponse(200, result.Items);
+  
+    } catch (error) {
+      console.log('Error retrieving workouts:', error);
+  
+      return util.buildResponse(500, { message: 'Internal Server Error' });
     }
-
-    // Return a 200 response with all workouts
-    return util.buildResponse(200, result.Items);
-
-  } catch (error) {
-    console.log('Error retrieving workouts:', error);
-
-    // Return a 500 response in case of any internal server error
-    return util.buildResponse(500, { message: 'Internal Server Error' });
   }
-}
 
 module.exports.getAllWorkouts = getAllWorkouts;
