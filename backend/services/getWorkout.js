1dfb31e4e7dd3948ee0fbe5519f8be021fcc187e
Steven <stevenpstansberry@gmail.com>
2024-08-21 20:50:02 -0700

    edited getWorkout

--- backend/services/getWorkout.js
+++ backend/services/getWorkout.js
@@ -1,23 +1,34 @@
 const AWS = require('aws-sdk');
 AWS.config.update({
   region: 'us-east-1'
-})
+});
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 const workoutTable = 'fit-graph-user-workouts';
+const util = require('../utils/util');
 
-async function getWorkoutById(workoutId){
-    const params = {
-        TableName : workoutTable,
-        Key: {
-            workoutId : workoutId
-        }
-    };
-    return await dynamodb.get(params).promise().then( response => {
-        return response.Item;
-    }, error => {
-        console.log('error', error);
-    })
+async function getWorkoutById(workoutId) {
+  const params = {
+    TableName: workoutTable,
+    Key: {
+      workoutId: workoutId
+    }
+  };
+
+  try {
+    const result = await dynamodb.get(params).promise();
+
+    if (!result.Item) {
+      return util.buildResponse(404, { message: `Workout with ID ${workoutId} not found` });
+    }
+
+    return util.buildResponse(200, result.Item);
+
+  } catch (error) {
+    console.log('Error retrieving workout:', error);
+
+    return util.buildResponse(500, { message: 'Internal Server Error' });
+  }
 }
 
-module.exports.getWorkoutById = getWorkoutById
+module.exports.getWorkoutById = getWorkoutById;
