a401eaddda93ebfddd32f517efcb4b8c24bb9134
Steven <stevenpstansberry@gmail.com>
2024-09-06 23:46:27 -0700

    added ViewWorkouts.js

---
+++ my-app/src/components/ViewWorkouts.js
@@ -0,0 +1,7 @@
+/**
+ * Component for viewing workouts, including filtering by month and year, and adding workouts.
+ * 
+ * @component
+ * @param {Object} props - The properties passed to the component.
+ * @returns {React.Element} - The rendered component.
+ */
