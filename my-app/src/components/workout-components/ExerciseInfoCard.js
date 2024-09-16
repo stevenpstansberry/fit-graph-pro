bef98b4bc1fdb64db72e12ba7fa8ede44c1cc5e3
Steven <stevenpstansberry@gmail.com>
2024-09-16 13:40:17 -0700

    Added documentation to ExerciseInfoCard

--- my-app/src/components/workout-components/ExerciseInfoCard.js
+++ my-app/src/components/workout-components/ExerciseInfoCard.js
@@ -1,9 +1,28 @@
+/**
+ * @fileoverview Component the includes a description of the exercise, a how to guide, and an animation of the exercise.
+ *
+ * @file src/components/workout-components/ExerciseInfoCard.js
+ *
+ * Utulizes the ExerciseDB API for information
+ *
+ * @version 1.0.0
+ * @author Steven Stansberry
+ */
 
 import React, { useState, useEffect } from "react";
 import { Card, Modal } from "@mui/material";
 
 
-
+/**
+ * Component for rendering and managing an exercise card within a workout.
+ * 
+ * @component
+ * @param {Object} props - The properties passed to the component.
+ * @param {boolean} props.open - Flag indicating whether the modal is open.
+ * @param {function} props.onClose - Function to close the modal.
+ * @param {Object} props.exercise - The exercise object containing label, bodyPart, and sets. 
+ * @returns {React.Element} - The rendered ExerciseInfoCard component.
+ */
 function ExerciseInfoCard({ open, onClose, exercise }) {
  
 
